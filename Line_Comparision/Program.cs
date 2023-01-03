401bbd9e8221fc2264f03730bbc240e52c027c0b
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-03 18:26:21 +0530

    [Rushi]Welcome

---
+++ Line_Comparision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Line_Comparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WEL - COME to Line Comparision ");
+        }
+    }
+}
