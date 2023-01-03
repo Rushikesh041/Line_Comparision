8e9af54f0b2233fcf9b5ae86dbb09e6d9e0a1b58
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-03 21:10:17 +0530

    [Rushi]UC2_EqualorNot

--- Line_Comparision/CalculateCompare.cs
+++ Line_Comparision/CalculateCompare.cs
@@ -8,6 +8,7 @@ namespace Line_Comparision
 {
     public class CalculateCompare
     {
+        public int X1, X2, Y1, Y2;
         public CalculateCompare(int x1, int x2, int y1, int y2)
         {
             X1 = x1;
@@ -15,6 +16,45 @@ namespace Line_Comparision
             Y1 = y1;
             Y2 = y2;
         }
+        public static CalculateCompare UserInputForLine1()
+        {
+            Console.WriteLine("Enter 1st Line Co-Ordinates");
+            Console.Write("Enter X1 Co-ordinate : ");
+            int xx1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter X2 Co-ordinate : ");
+            int xx2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y1 Co-ordinate : ");
+            int yy1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y2 Co-ordinate : ");
+            int yy2 = Convert.ToInt32(Console.ReadLine());
+
+            return new CalculateCompare(xx1, xx2, yy1, yy2)
+            {
+                X1 = xx1,
+                X2 = xx2,
+                Y1 = yy1,
+                Y2 = yy2
+            };
+        }
+        public static CalculateCompare UserInputForLine2()
+        {
+            Console.WriteLine("Enter 2nd Line Co-Ordinates");
+            Console.Write("Enter X1 Co-ordinate : ");
+            int xl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter X2 Co-ordinate : ");
+            int xl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y1 Co-ordinate : ");
+            int yl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y2 Co-ordinate : ");
+            int yl2 = Convert.ToInt32(Console.ReadLine());
+
+            return new CalculateCompare(xl1, xl2, yl1, yl2)
             {
                 X1 = xl1,
                 X2 = xl2,
--- Line_Comparision/Program.cs
+++ Line_Comparision/Program.cs
@@ -8,23 +8,10 @@
             CalculateCompare line = new CalculateCompare(obj.X1, obj.X2, obj.Y1, obj.Y2);
             double length1 = line.CalculateLengthOfLine();
 
-            var obj1 =CalculateCompare.UserInputForLine2();
+            var obj1 = CalculateCompare.UserInputForLine2();
             CalculateCompare line1 = new CalculateCompare(obj1.X1, obj1.X2, obj1.Y1, obj1.Y2);
             double length2 = line1.CalculateLengthOfLine();
 
-<<<<<<< HEAD
-            Console.Write("Enter X2 Co-ordinate : ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter Y1 Co-ordinate : ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter Y2 Co-ordinate : ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-
-            CalculateCompare line = new CalculateCompare(x1 , x2, y1, y2);
-            line.CalculateLengthOfLine();
-=======
             if (length1 == length2)
             {
                 Console.WriteLine("Length of Two Lines are Same");
@@ -33,7 +20,7 @@
             {
                 Console.WriteLine("Length of Two Lines are Not Same");
             }
->>>>>>> UC2_EqualorNot
+
         }
     }
 }
