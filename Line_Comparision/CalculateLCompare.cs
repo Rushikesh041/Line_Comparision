772f0115443d95ac670b16e362f74599f74aba6d
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-03 19:09:16 +0530

    [Rushi]UC1_CalculateLength

---
+++ Line_Comparision/CalculateLCompare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    internal class CalculateLength
+    {
+    }
+}
--- Line_Comparision/Program.cs
+++ Line_Comparision/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WEL - COME to Line Comparision ");
+
+            Console.Write("Enter X1 Co-ordinate : ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter X2 Co-ordinate : ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y1 Co-ordinate : ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Y2 Co-ordinate : ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            CalculateLCompare line = new CalculateLCompare(x1 , x2, y1, y2);
+            line.CalculateLengthOfLine();
         }
     }
 }
